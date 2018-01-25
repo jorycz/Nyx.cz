08266e706359da48f6659048996bb25a3ce73bb5
Josef Rysanek <josef.rysanek@gmail.com>
2018-01-25 18:17:43 +0100

    Better animation for dark theme.

--- Nyx.cz/UI/Common/LoadingView.m
+++ Nyx.cz/UI/Common/LoadingView.m
@@ -27,8 +27,9 @@
 //        spinner.center = self.center;
         
         // Image animation
-        NSArray *imageNames = @[@"0.png", @"1.png", @"2.png", @"3.png", @"4.png", @"5.png", @"6.png", @"7.png", @"8.png", @"9.png", @"10.png",
-                            @"11.png", @"12.png", @"13.png", @"14.png", @"15.png", @"16.png", @"17.png", @"18.png", @"19.png", @"20.png", @"21.png"];
+        NSArray *imageNames = @[@"01.png", @"02.png", @"03.png", @"04.png", @"05.png", @"06.png", @"07.png", @"08.png", @"09.png", @"10.png", @"11.png",
+                            @"12.png", @"13.png", @"14.png", @"15.png", @"16.png", @"17.png", @"18.png", @"19.png", @"20.png", @"21.png", @"22.png",
+                                @"23.png", @"24.png", @"25.png", @"26.png", @"27.png", @"28.png", @"29.png", @"30.png"];
         
         NSMutableArray *images = [[NSMutableArray alloc] init];
         for (int i = 0; i < imageNames.count; i++) {
